a596878559103b7b3ae4826b91d7cc9b3f6e2a28
Trusaman <ngocanha85@gmail.com>
2022-06-12 20:49:49 -0400

    update guess number - initial

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,12 @@
-'use strict';
+// Create random number
+let randomNumber = Math.floor(Math.random() * 20) + 1
+
+let guessNumber = document.getElementById("guess-number")
+
+let checkBtn = document.getElementById("check-btn")
+
+// display guess number when click
+const displayGuess = function() {
+    console.log("Guess: ", guessNumber.value)
+}
+checkBtn.addEventListener("click", displayGuess)
